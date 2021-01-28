e2604777752072f78d71830b534f25212ba750dc
Brandon Edwards <brandon.edwards144@gmail.com>
2021-01-28 08:52:02 -0500

    Save list of distance plots as .rda object and refer to them by name.

--- src/plot-distance.R
+++ src/plot-distance.R
@@ -24,6 +24,8 @@ ibp <- read.csv("../utilities/IBP-Alpha-Codes20.csv")[, c("SPEC",
 ####### Plot All Species with Mean ################
 
 forest_level <- c(1.0, 0.0)
+
+# Create a variable where you can separate roadside status but species
 sim_data$SpeciesRoad <- paste0(sim_data$Species, sim_data$Roadside)
 
 # Empty plot list
@@ -56,7 +58,7 @@ dev.off()
 ####### Plot by Species ###########################
 
 sm_list <- vector(mode = "list", length = length(unique(sim_data$Species)))
-sm <- 1
+names(sm_list) <- unique(sim_data$Species)
 
 for (sp in unique(sim_data$Species))
 {
@@ -88,66 +90,11 @@ for (sp in unique(sim_data$Species))
                    sp)
   )
   
-  sm_list[[sm]] <- plot_matrix
-  sm <- sm + 1  
+  sm_list[[sp]] <- plot_matrix
 }
+
+save(sm_list, file = "plots/distance/distance_plot_list.rda")
 
 pdf(file = paste0("plots/distance/distance_species.pdf"))
 print(sm_list)
 dev.off() 
-
-
-# ####### Plot by Family ############################
-# 
-# # Add Family grouping
-# family <- merge(x = family, y = ibp,
-#                 by.x = "common_name", by.y = "COMMONNAME")
-# sim_data <- merge(x = sim_data, y = family[, c("SPEC", "family")],
-#                   by.x = "Species", by.y = "SPEC")
-# 
-# pm_list <- vector(mode = "list", length = length(unique(sim_data$family)))
-# pm <- 1
-# 
-# for (f in sort(unique(sim_data$family)))
-# {
-#   # Empty plot list
-#   plot_list <- vector(mode = "list", length = length(radius_values) * length(unique(roadside)))
-#   
-#   i <- 1
-#   n_sp <- length(unique(sim_data[which(sim_data$family == f), "Species"]))
-#   for (rad in radius_values)
-#   {
-#     for (road in c(1, 0))
-#     {
-#       plot_list[[i]] <- 
-#         ggplot(data = sim_data[which(sim_data$Roadside == road & 
-#                                        sim_data$Radius == rad &
-#                                        sim_data$family == f),]) +
-#         geom_line(aes(x = Forest, y = q, group = Species), alpha = 0.2) +
-#         stat_summary(aes(x = Forest, y = q), fun = mean, geom = "smooth", size = 1.25) +
-#         ylim(0, 1) +
-#         theme(legend.position = "none")
-#       i <- i + 1
-#     }
-#   }
-#   
-#   plot_matrix <- ggmatrix(
-#     plot_list,
-#     ncol = length(unique(roadside)),
-#     nrow = length(radius_values),
-#     xAxisLabels = c("On-Road Survey", "Off-road Survey"),
-#     yAxisLabels = c("50m", "100m", "200m", "400m"),
-#     title = paste0("Family ",
-#                    f,
-#                    " (n = ",
-#                    n_sp,
-#                    ")")
-#   )
-#   
-#   pm_list[[pm]] <- plot_matrix
-#   pm <- pm + 1
-# }
-# 
-# pdf(file = paste0("plots/distance/distance_families.pdf"))
-# print(pm_list)
-# dev.off() 
