45f9aceeaefd483f1ef34fc11925513c6410137e
Brandon Edwards <brandon.edwards144@gmail.com>
2021-04-22 15:24:51 -0400

    paper specific plots

--- src/7-plot-distance.R
+++ src/7-plot-distance.R
@@ -13,6 +13,10 @@ library(ggpubr)
 library(viridis)
 theme_set(theme_pubclean())
 
+####### Set Constants #############################
+
+sp_figure <- "CARW"
+
 ####### Read Data #################################
 
 # "4-simulate-tau.R" MUST BE RUN before trying to read in tau_sim.csv!!
@@ -43,42 +47,6 @@ sim_data$q_97.5 <- ifelse(sim_data$Radius == "Inf",
                           ((sim_data$tau_97.5 ^ 2) / (sim_data$Radius ^ 2)) *
                             (1 - exp(-(sim_data$Radius ^ 2) /
                                        sim_data$tau_97.5 ^ 2)))
-
-
-
-# ####### Plot All Species with Mean ################
-# 
-# forest_level <- c(1.0, 0.0)
-# 
-# # Create a variable where you can separate roadside status but species
-# sim_data$SpeciesRoad <- paste0(sim_data$Species, sim_data$Roadside)
-# 
-# # Empty plot list
-# plot_list <- vector(mode = "list", length = length(forest_level))
-# i <- 1
-# 
-# for (fc in c(1.0, 0.0))
-# {
-#   plot_list[[i]] <- 
-#     ggplot(data = sim_data[which(sim_data$Forest == fc),]) +
-#     geom_line(aes(x = Radius, y = q, group = SpeciesRoad, color = as.factor(Roadside)), alpha = 0.05) +
-#     stat_summary(aes(x = Radius, y = q, group = as.factor(Roadside), color = as.factor(Roadside)), fun = mean, geom = "smooth", size = 1.25) +
-#     ylim(0, 1) +
-#     #theme(legend.position = "none") +
-#     NULL
-#   i <- i + 1  
-# }
-# 
-# plot_matrix <- ggmatrix(
-#   plot_list,
-#   ncol = length(forest_level),
-#   nrow = 1,
-#   xAxisLabels = c("Forest", "Non-forest")
-# )
-# png(filename = "plots/distance/distance_all_species.png",
-#     width = 7, height = 4, units = "in", res = 300)
-# print(plot_matrix)
-# dev.off()
 
 ####### Plot by Species ###########################
 forest_level <- c(1.0, 0.0)
@@ -133,3 +101,16 @@ save(sm_list, file = "output/plots/distance/distance_plot_list.rda")
 pdf(file = paste0("output/plots/distance/distance_species.pdf"))
 print(sm_list)
 dev.off() 
+
+####### Paper Figure ##############################
+figure <- sm_list[[sp_figure]]
+figure$title <- NULL
+figure$xlab <- "Survey Distance"
+figure$ylab <- "q"
+
+png(filename = paste0("output/plots/distance/",
+                      sp_figure,
+                      "_figure.png"),
+    width = 6, height = 4, units = "in", res = 300)
+print(figure)
+dev.off()
--- src/8-plot-removal-no-bcr.R
+++ src/8-plot-removal-no-bcr.R
@@ -10,11 +10,17 @@
 library(ggplot2)
 library(GGally)
 library(ggpubr)
+library(viridis)
 theme_set(theme_pubclean())
+
+####### Set Constants #############################
+
+sp_figure <- "GHOW"
 
 ####### Read Data #################################
 
 sim_data <- read.csv("output/tables/phi_sim_no_bcr.csv")
+load("output/tables/phi_sim_bcr_list.rda")
 
 ####### Simulate p ################################
 
@@ -28,77 +34,10 @@ sim_data$p <- 1 - exp(-(sim_data$Time * sim_data$phi))
 sim_data$p_2.5 <- 1 - exp(-(sim_data$Time * sim_data$phi_2.5))
 sim_data$p_97.5 <- 1 - exp(-(sim_data$Time * sim_data$phi_97.5))
 
-# ####### Plot All Species with Mean (JD) ###########
-# 
-# # Hold TSSR (we'll call it sr here) constant at 1
-sr <- 1
-# 
-# # Empty plot list
-# plot_list <- vector(mode = "list", length = length(time_values))
-# 
-# i <- 1
-# 
-# for (tv in time_values)
-# {
-#   plot_list[[i]] <- 
-#     ggplot(data = sim_data[which(sim_data$TSSR == sr & 
-#                                    sim_data$Time == tv),]) +
-#     geom_line(aes(x = JD, y = p, group = Species), alpha = 0.05) +
-#     stat_summary(aes(x = JD, y = p), fun = mean, geom = "smooth", size = 1.25) +
-#     ylim(0, 1) +
-#     #theme(legend.position = "none") +
-#     NULL
-#   i <- i + 1
-# }
-# 
-# plot_matrix <- ggmatrix(
-#   plot_list,
-#   ncol = length(time_values),
-#   nrow = 1,
-#   xAxisLabels = c("1 min", "3 min", "5 min", "10 min")
-# )
-# 
-# png(filename = "plots/removal/removal_jd_all_species.png",
-#     width = 7, height = 3, units = "in", res = 300)
-# print(plot_matrix)
-# dev.off()
-# 
-# ####### Plot All Species with Mean (TSSR) ###########
-# 
-# # Hold JD (we'll call it day here) constant at 152 (i.e. June 1)
-day <- 152
-# 
-# # Empty plot list
-# plot_list <- vector(mode = "list", length = length(time_values))
-# 
-# i <- 1
-# 
-# for (tv in time_values)
-# {
-#   plot_list[[i]] <- 
-#     ggplot(data = sim_data[which(sim_data$JD == day & 
-#                                    sim_data$Time == tv),]) +
-#     geom_line(aes(x = TSSR, y = p, group = Species), alpha = 0.05) +
-#     stat_summary(aes(x = TSSR, y = p), fun = mean, geom = "smooth", size = 1.25) +
-#     ylim(0, 1) +
-#     #theme(legend.position = "none") +
-#     NULL
-#   i <- i + 1
-# }
-# 
-# plot_matrix <- ggmatrix(
-#   plot_list,
-#   ncol = length(time_values),
-#   nrow = 1,
-#   xAxisLabels = c("1 min", "3 min", "5 min", "10 min")
-# )
-# 
-# png(filename = "plots/removal/removal_tssr_all_species.png",
-#     width = 7, height = 3, units = "in", res = 300)
-# print(plot_matrix)
-# dev.off()
-
 ####### Plot by Species (JD) ######################
+
+# Hold TSSR (we'll call it sr here) constant at 1
+sr <- 1
 
 sm_list <- vector(mode = "list", length = length(unique(sim_data$Species)))
 names(sm_list) <- unique(sim_data$Species)
@@ -119,9 +58,10 @@ for (sp in unique(sim_data$Species))
       geom_line(aes(x = JD, y = p)) +
       geom_ribbon(aes(x = JD, ymin = p_2.5, ymax = p_97.5),
                   alpha = 0.25) +
-      #stat_summary(aes(x = JD, y = p), fun = mean, geom = "smooth", size = 1.25) +
       ylim(0, 1) +
-      theme(legend.position = "none")
+      theme(legend.position = "none") +
+      scale_color_viridis(discrete=TRUE) +
+      NULL
     i <- i + 1
   }
   
@@ -137,6 +77,8 @@ for (sp in unique(sim_data$Species))
   sm_list[[sp]] <- plot_matrix
 }
 
+jd_figure <- sm_list[[sp_figure]]
+
 save(sm_list, file = "output/plots/removal/removal_jd_plot_list.rda")
 
 pdf(file = paste0("output/plots/removal/removal_jd_species.pdf"), width = 7, height = 3)
@@ -144,6 +86,9 @@ print(sm_list)
 dev.off() 
 
 ####### Plot by Species (TSSR) ####################
+
+# Hold JD (we'll call it day here) constant at 152 (i.e. June 1)
+day <- 152
 
 sm_list <- vector(mode = "list", length = length(unique(sim_data$Species)))
 names(sm_list) <- unique(sim_data$Species)
@@ -166,7 +111,9 @@ for (sp in unique(sim_data$Species))
                   alpha = 0.25) +
      # stat_summary(aes(x = TSSR, y = p), fun = mean, geom = "smooth", size = 1.25) +
       ylim(0, 1) +
-      theme(legend.position = "none")
+      theme(legend.position = "none") +
+      scale_color_viridis(discrete=TRUE) +
+      NULL
     i <- i + 1
   }
   
@@ -182,8 +129,34 @@ for (sp in unique(sim_data$Species))
   sm_list[[sp]] <- plot_matrix
 }
 
+tssr_figure <- sm_list[[sp_figure]]
+
 save(sm_list, file = "output/plots/removal/removal_tssr_plot_list.rda")
 
 pdf(file = paste0("output/plots/removal/removal_tssr_species.pdf"), width = 7, height = 3)
 print(sm_list)
 dev.off() 
+
+####### Paper Figure ##############################
+
+jd_figure$title <- NULL
+jd_figure$xlab <- "Julian Day"
+jd_figure$ylab <- "p"
+
+png(filename = paste0("output/plots/removal/",
+                      sp_figure,
+                      "_jd.png"),
+    width = 6, height = 3, units = "in", res = 300)
+print(jd_figure)
+dev.off()
+
+tssr_figure$title <- NULL
+tssr_figure$xlab <- "Time Since Local Sunrise"
+tssr_figure$ylab <- "p"
+
+png(filename = paste0("output/plots/removal/",
+                      sp_figure,
+                      "_tssr.png"),
+    width = 6, height = 3, units = "in", res = 300)
+print(tssr_figure)
+dev.off()
