c31132667249a9f3497f331ba3ec8988a3d1a918
Brandon Edwards <brandon.edwards144@gmail.com>
2021-04-22 15:22:58 -0400

    Create combined boxplot, and data frame of models chosen if interaction models weren't availabile

--- src/1-model-selection.R
+++ src/1-model-selection.R
@@ -9,6 +9,7 @@
 
 library(ggplot2)
 library(ggpubr)
+library(viridis)
 theme_set(theme_pubclean())
 
 source("../utilities/rm-non-sp.R")
@@ -39,6 +40,7 @@ for (sp in dis_best$Species)
 rem_best <- as.data.frame(matrix(data = NA,
                                  nrow = length(unique(removal$Species)),
                                  ncol = ncol(removal)))
+
 names(rem_best) <- names(removal)
 rem_best$Species <- unique(removal$Species)
 
@@ -49,6 +51,24 @@ for (sp in rem_best$Species)
   min_aic <- temp[which(temp$aic == min(temp$aic, na.rm = TRUE)), ]
   rem_best[which(rem_best$Species == sp), ] <- min_aic
 }
+
+#' For all species that had a JD-BCR interaction, calculate what
+#' the best model WOULD have been without the interaction models
+#' being an option
+jd_bcr_sp <- rem_best[which(rem_best$model > 9), "Species"]
+
+rem_best_without_bcr <- as.data.frame(matrix(data = NA,
+                                             nrow = length(jd_bcr_sp),
+                                             ncol = ncol(removal)))
+names(rem_best_without_bcr) <- names(removal)
+rem_best_without_bcr$Species <- jd_bcr_sp
+for (sp in jd_bcr_sp)
+{
+  temp <- removal[which(removal$Species == sp), ]
+  min_aic <- temp[which(temp$aic[1:9] == min(temp$aic[1:9], na.rm = TRUE)), ]
+  rem_best_without_bcr[which(rem_best_without_bcr$Species == sp), ] <- min_aic
+}
+
 
 ####### Create Table ##############################
 
@@ -87,6 +107,25 @@ rem_box <- ggplot(data = rem_models) +
   xlab("Model Complexity") +
   ylab("Sample Size") +
   NULL
+
+####### Boxplot Side-by-Side by Sample Size #######
+
+combined <- data.frame(Model = rep("Removal", nrow(rem_models)),
+                       n = rem_models$n,
+                       Complexity = rem_models$Complexity)
+combined <- rbind(combined,
+                  data.frame(Model = rep("Distance", nrow(dis_models)),
+                             n = dis_models$n,
+                             Complexity = dis_models$Complexity))
+
+combined$Model <- factor(combined$Model, levels = c("Removal", "Distance"))
+
+combined_box <- ggplot(data = combined) + 
+  geom_boxplot(aes(x = Complexity, y = n, fill = Model)) +
+  xlab("Model Complexity") +
+  ylab("Sample Size") +
+  scale_fill_viridis(discrete=TRUE) +
+  NULL
 ####### Output Results ############################
 
 write.table(rem_best, file = "output/tables/removal_best.csv", sep = ",", row.names = FALSE)
@@ -101,4 +140,9 @@ dev.off()
 png(filename = "output/plots/rem_model_box.png", 
     width = 6, height = 6, units = "in", res = 300)
 print(rem_box)
+dev.off()
+
+png(filename = "output/plots/combined_model_box.png",
+    width = 6, height = 6, units = "in", res = 300)
+print(combined_box)
 dev.off()
