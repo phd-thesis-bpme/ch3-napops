fc5ed0aa8a468f4b9c89b765cbe257d9bf109b35
Brandon Edwards <brandon.edwards144@gmail.com>
2022-01-03 19:39:13 -0500

    use napops libbrary to get coefficients

--- src/02-coefficient-table.R
+++ src/02-coefficient-table.R
@@ -3,9 +3,11 @@
 # NA-POPS: NA-POPS-paper-2021
 # 2-coefficient-table.R
 # Created January 2021
-# Last Updated October 2021
+# Last Updated December 2021
 
 ####### Import Libraries and External Files #######
+
+library(napops)
 
 source("../utilities/rm-non-sp.R")
 
@@ -15,29 +17,26 @@ sp <- c("AMRO")
 
 ####### Read Data #################################
 
-distance <- rm_non_sp(read.csv("../results/coefficients/distance.csv"))
-removal <- rm_non_sp(read.csv("../results/coefficients/removal.csv"))
+dis_sp <- dis_coef(species = sp)
+rem_sp <- rem_coef(species = sp)
 
-####### Create Subsetted Tables ###################
+####### Order Tables by AIC and Add Delta #########
 
-dis_red <- distance[which(distance$Species %in% sp), ]
-dis_red <- dis_red[order(dis_red$aic), ]
-dis_red$aic <- dis_red$aic - dis_red$aic[1]
+dis_sp <- dis_sp[order(dis_sp$AIC), ]
+dis_sp$AIC <- dis_sp$AIC - dis_sp$AIC[1]
 
-rem_red <- removal[which(removal$Species %in% sp &
-                           removal$model <= 9), ]
-rem_red <- rem_red[order(rem_red$aic), ]
-rem_red$aic <- rem_red$aic - rem_red$aic[1]
+rem_sp <- rem_sp[order(rem_sp$AIC), ]
+rem_sp$AIC <- rem_sp$AIC - rem_sp$AIC[1]
 
 ####### Output Tables #############################
 
-write.table(x = dis_red,
+write.table(x = dis_sp,
             file = paste0("output/tables/distance_",
                           sp,
                           ".csv"),
             sep = ",", row.names = FALSE)
 
-write.table(x = rem_red,
+write.table(x = rem_sp,
             file = paste0("output/tables/removal_",
                           sp,
                           ".csv"),
