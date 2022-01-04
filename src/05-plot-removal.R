3e8ffcc4c8c948f00741be93fc99bfa805349eca
Brandon Edwards <brandon.edwards144@gmail.com>
2022-01-03 19:40:05 -0500

    change from julian day to ordinal day

--- src/05-plot-removal.R
+++ src/05-plot-removal.R
@@ -3,7 +3,7 @@
 # NA-POPS: NA-POPS-paper-2021
 # 5-plot-removal.R
 # Created January 2021
-# Last Updated October 2021
+# Last Updated December 2021
 
 ####### Import Libraries and External Files #######
 
@@ -24,7 +24,7 @@ best_mod <- read.csv(paste0("output/tables/removal_",
                             ".csv"))[1,"model"]
 load(paste0("../results/simulations/phi/phi_", best_mod, ".rda"))
 
-####### Plot by Species (JD) ######################
+####### Plot by Species (OD) ######################
 
 # Hold TSSR (we'll call it sr here) constant at 1
 sr <- 1
@@ -48,8 +48,8 @@ for (tv in time_values)
     ggplot(data = sim_data[which(sim_data$TSSR == sr & 
                                    sim_data$Time == tv &
                                    sim_data$Species == sp),]) +
-    geom_line(aes(x = JD, y = p, color = as.factor(1))) +
-    geom_ribbon(aes(x = JD, ymin = p_2.5, ymax = p_97.5, color = NA),
+    geom_line(aes(x = OD, y = p, color = as.factor(1))) +
+    geom_ribbon(aes(x = OD, ymin = p_2.5, ymax = p_97.5, color = NA),
                 alpha = 0.25) +
     ylim(0, 1) +
     theme(legend.position = "none") +
@@ -58,7 +58,7 @@ for (tv in time_values)
   i <- i + 1
 }
 
-jd_figure <- ggmatrix(
+od_figure <- ggmatrix(
   plot_list,
   ncol = length(time_values),
   nrow = 1,
@@ -67,21 +67,21 @@ jd_figure <- ggmatrix(
 
 #sm_list[[sp]] <- plot_matrix
 
-jd_figure$title <- NULL
-jd_figure$xlab <- "Ordinal Day"
-jd_figure$ylab <- "Availability (p)"
+od_figure$title <- NULL
+od_figure$xlab <- "Ordinal Day"
+od_figure$ylab <- "Availability (p)"
 
 png(filename = paste0("output/plots/removal/",
                       sp,
-                      "_jd.png"),
+                      "_od.png"),
     width = 6, height = 3, units = "in", res = 300)
-print(jd_figure)
+print(od_figure)
 dev.off()
 
 ####### Plot by Species (TSSR) ####################
 
-# Hold JD (we'll call it day here) constant at 152 (i.e. June 1)
-day <- 152
+# Hold OD (we'll call it day here) constant at 160 (i.e. June 9)
+day <- 160
   
 # Empty plot list
 plot_list <- vector(mode = "list", length = length(time_values))
@@ -91,7 +91,7 @@ i <- 1
 for (tv in time_values)
 {
   plot_list[[i]] <- 
-    ggplot(data = sim_data[which(sim_data$JD == day & 
+    ggplot(data = sim_data[which(sim_data$OD == day & 
                                    sim_data$Time == tv &
                                    sim_data$Species == sp),]) +
     geom_line(aes(x = TSSR, y = p, color = as.factor(1))) +
@@ -126,6 +126,6 @@ print(tssr_figure)
 dev.off()
 
 par(mfrow = c(2,1))
-jd_figure
+od_figure
 tssr_figure
 
