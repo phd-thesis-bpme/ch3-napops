a634fec6b98d8781c7b94792c3ae8c3ec05c8080
Brandon Edwards <brandon.edwards144@gmail.com>
2022-01-21 13:29:08 -0500

    FIgure 2 script

--- src/figure-02-covariate-space.R
+++ src/figure-02-covariate-space.R
@@ -12,6 +12,7 @@ library(ggridges)
 library(viridis)
 library(ggpubr)
 library(napops)
+library(magrittr)
 theme_set(theme_pubclean())
 
 ####### Read Data #################################
@@ -40,12 +41,36 @@ dis_plot <- ggplot(data = dis_covars, aes(x = Forest, fill = Roadside)) +
   theme(legend.position="bottom") +
   NULL
 
+####### Generate Distance Max Radius Bins #########
+dis_covars$Max_Distance <- max_survey_radius(code = dis_covars$Survey_Method) %>%
+  factor(levels = c("30", "75", "100", "150", "400", "Inf"))
+
+radius_bins <- ggplot(data = dis_covars, aes(x = Max_Distance)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = -0.5) +
+  xlab("Maximum Survey Radius (m)") +
+  ylab("Count") +
+  ylim(c(0,481000)) +
+  NULL
+
+####### Generate Removal Max Time Bins ############
+rem_covars$Max_Time <- max_survey_time(code = rem_covars$Survey_Method) %>%
+  factor(levels = c("3", "5", "6", "8", "10"))
+
+time_bins <- ggplot(data = rem_covars, aes(x = Max_Time)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = -0.5) +
+  xlab("Maximum Survey Time (min)") +
+  ylab("Count") +
+  ylim(c(0,198000)) +
+  NULL
+
 ####### Output Composite Figure ###################
 
-png("output/plots/coverage_covariates.png",
-    width = 6, height = 8, units = "in", res = 300)
-ggarrange(mp, 
-          ggarrange(rem_plot, dis_plot, ncol = 2, labels = c("b)", "c)")),
-          nrow = 2,
-          labels = "a)")
+png("output/plots/Fig2-covariates.png",
+    width = 7.5, height = 7.5, units = "in", res = 1200)
+ggarrange(rem_plot, dis_plot, 
+          time_bins, radius_bins,
+          ncol = 2, nrow = 2,
+          labels = c("A", "B", "C", "D"))
 dev.off()
